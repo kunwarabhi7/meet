77471ca51192318b4c5b026b57745f77c5f3bcd8
Abhishek <abhishekparmar162@gmail.com>
2025-06-20 18:36:25 +0530

    modified frontent

--- client/src/utils/axionInstance.jsx
+++ client/src/utils/axionInstance.jsx
@@ -1,9 +1,11 @@
 import axios from "axios";
 
-const BASE_URL =
-  import.meta.env.MODE === "development"
-    ? import.meta.env.VITE_DEV_API
-    : import.meta.env.VITE_PROD_API;
+// const BASE_URL =
+//   import.meta.env.MODE === "development"
+//     ? import.meta.env.VITE_DEV_API
+//     : import.meta.env.VITE_PROD_API;
+
+const BASE_URL = import.meta.env.VITE_PROD_API;
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
